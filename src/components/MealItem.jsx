0e67088e69b24da9d9dc3ce358ebe8cffe224d73
Nieblas <nieblasleonel@gmail.com>
2024-02-22 18:21:15 -0600

    custom button created

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {currencyFormatter} from '../utils/formatting.jsx';
+import { Button } from './UI/Button.jsx';
 
 export const MealItem = ({meal}) => {
   return (
@@ -7,11 +9,11 @@ export const MealItem = ({meal}) => {
             <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
             <div>
             <h3>{meal.name}</h3>
-            <p className='meal-item-price'>{meal.price}</p>
+            <p className='meal-item-price'>{currencyFormatter.format(meal.price)}</p>
             <p className='meal-item-description'>{meal.description}</p>
             </div>
             <p className='meal-item-actions'>
-                <button>Add to Cart</button>
+                <Button>Add to Cart</Button>
             </p>
         </article>
     </li>
